dfa5bee008bd3d9e1cf108966e1a0d677b440c59
Marcus Pontes <marcusporama@gmail.com>
2021-01-20 16:59:43 -0300

    Ajuste no componente app e no componente cadastro para utilização dos campos

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,74 @@
-import React, { Component } from "react"
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import { FiTrash2 } from "react-icons/fi"
+import { FaWhmcs } from "react-icons/fa"
+
 import NavBar from "../Navbar/NavBar"
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-      </div>
-    )
-  }
-}
+import api from "../services/api"
 
-export default App
+export default function Cliente() {
+  const [clientes, setClientes] = useState([])
+
+  useEffect(() => {
+    api.get("profile/cliente", {}).then((response) => {
+      setClientes(response.data)
+    })
+  })
+
+  async function handleDeleteProd(id) {
+    try {
+      await api.delete(`clientes/${id}`, {
+        headers: {},
+      })
+
+      setClientes(clientes.filter((cliente) => cliente.id !== id))
+    } catch (erro) {
+      alert("Erro ao deletar Cliente, tente novamente.")
+    }
+  }
+
+  function salvar(id) {
+    localStorage.setItem("idClie", id)
+  }
+
+  return (
+    <div className="profile-container">
+      <NavBar />
+
+      <h1>Clientes Cadastrados</h1>
+
+      <ul>
+        {clientes.map((cliente) => (
+          <li key={cliente.id}>
+            <strong>NOME DO CLIENTE :</strong>
+            <p>{cliente.nome}</p>
+            <strong>CPF DO CLIENTE :</strong>
+            <p>{cliente.cpf}</p>
+            <strong>TELEFONE DO CLIENTE :</strong>
+            <p>{cliente.numero}</p>
+            <strong>ENDEREÇO DO CLIENTE :</strong>
+            <p>{cliente.endereco}</p>
+            <strong>EMAIL DO CLIENTE :</strong>
+            <p>{cliente.email}</p>
+            <strong>SEXO DO CLIENTE :</strong>
+            <p>{cliente.sexo}</p>
+            <strong>NASCIMENTO DO CLIENTE :</strong>
+            <p>{cliente.nacimento}</p>
+            <Link
+              to="/cliente-editar"
+              onClick={() => salvar(cliente.id)}
+              className="button2"
+              type="button"
+            >
+              <FaWhmcs size={20} color="#a8a8b3"></FaWhmcs>
+            </Link>
+            <button onClick={() => handleDeleteProd(cliente.id)} type="button">
+              <FiTrash2 size={20} color="#a8a8b3"></FiTrash2>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- src/Cadastro/index.js
+++ src/Cadastro/index.js
@@ -1,14 +1,139 @@
-import React, { Component } from "react"
-import NavBar from "../Navbar/NavBar"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import { FiArrowLeft } from "react-icons/fi"
+import api from "../../../services/api"
+import Modal from "../../../components/Modal"
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-      </div>
-    )
+import "./styles.css"
+
+export default function ClienteNew() {
+  const [nome, setNome] = useState("")
+  const [endereco, setEndereco] = useState("")
+  const [bairro, setBairro] = useState("")
+  const [cidade, setCidade] = useState("")
+  const [uf, setUf] = useState("")
+  const [telefone, setTelefone] = useState("")
+  const [email, setEmail] = useState("")
+
+  const [modalVisible, setModalVisible] = useState(false)
+  const [mensagemModal, setmensagemModal] = useState("")
+
+  async function newCliente(e) {
+    e.preventDefault()
+
+    if (nome === null || nome === "") {
+      setmensagemModal("Preencha o nome do Cliente")
+      setModalVisible(true)
+      return
+    }
+    if (endereco === null || endereco === "") {
+      setmensagemModal("Preencha o endereço corretamente")
+      setModalVisible(true)
+      return
+    }
+    if (bairro === null || bairro === "") {
+      setmensagemModal("Preencha o endereço corretamente")
+      setModalVisible(true)
+      return
+    }
+    if (cidade === null || cidade === "") {
+      setmensagemModal("Preencha o endereço corretamente")
+      setModalVisible(true)
+      return
+    }
+    if (uf === null || uf === "") {
+      setmensagemModal("Preencha o endereço corretamente")
+      setModalVisible(true)
+      return
+    }
+    if (telefone === null || telefone === "") {
+      setmensagemModal("Preencha o endereço corretamente")
+      setModalVisible(true)
+      return
+    }
+    if (email === null || email === "") {
+      setmensagemModal("Preencha o endereço corretamente")
+      setModalVisible(true)
+      return
+    }
+
+    const data = {
+      nome,
+      endereco,
+      bairro,
+      cidade,
+      uf,
+      telefone,
+      email,
+    }
+
+    try {
+      await api.post("clientes", data, {})
+
+      this.props.history.push("/app")
+    } catch (error) {
+      alert("Erro ao cadastrar Cliente")
+    }
   }
-}
 
-export default App
+  return (
+    <div>
+      {modalVisible ? (
+        <Modal onClose={() => setModalVisible(false)} title={mensagemModal} />
+      ) : null}
+      <div className="new-vaga-container">
+        <div className="conteiner">
+          <section>
+            <img src={logoImg} alt="Be The Hero" />
+            <h1>Cadastrar Cliente</h1>
+            <p>Cadastre o cliente e torneu membro da nossa equipe.</p>
+            <Link className="back-link" to="/clientes">
+              <FiArrowLeft size={16} color="#E02041" />
+              Voltar para Home
+            </Link>
+          </section>
+          <form onSubmit={newCliente}>
+            <input
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              placeholder="Nome do Cliente"
+            />
+            <input
+              value={endereco}
+              onChange={(e) => setEndereco(e.target.value)}
+              placeholder="Endereço do Cliente"
+            />
+            <input
+              value={bairro}
+              onChange={(e) => setBairro(e.target.value)}
+              placeholder="Bairro do Cliente"
+            />
+            <input
+              value={cidade}
+              onChange={(e) => setCidade(e.target.value)}
+              placeholder="Cidade do Cliente"
+            />
+            <input
+              value={uf}
+              onChange={(e) => setUf(e.target.value)}
+              placeholder="Uf do Cliente"
+            />
+            <input
+              value={telefone}
+              onChange={(e) => setTelefone(e.target.value)}
+              placeholder="Telefone do Cliente"
+            />
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="E-mail do Cliente"
+            />
+            <button className="button" type="submit">
+              Cadastrar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
